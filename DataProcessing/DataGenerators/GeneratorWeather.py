c4bad47bf01359bd2ad9ac3756637c222205fbc2
AKJauk <ann-kathrin.jauk@hs-furtwangen.de>
2021-11-12 11:11:07 +0100

    updated path for weather-file

--- DataProcessing/DataGenerators/GeneratorWeather.py
+++ DataProcessing/DataGenerators/GeneratorWeather.py
@@ -6,9 +6,8 @@ def generateWeatherData():
     #columnnames:   date, tavg (average temperature), tmin (min. temp.), tmax (max. temp.), 
     #               prcp (overall precipitation/Gesamtniederschlag), snow, wdir (wind direction),
     #               wspd (wind speed), wpgt (wind peak/Spitzenboe), pres (pressure/Luftdruck), 
-    #               tsun (time of sunshine) 
-
-    path = './DataProcessing/Datasets/Weather/weather_012019-102021.csv'
+    #               tsun (time of sunshine)
+    path = '../Datasets/Weather/weather_012019-102021.csv'
     data = pd.read_csv(path, header=[0])
     data = data.drop(columns=['prcp', 'snow', 'wdir', 'wspd', 'wpgt', 'pres', 'tsun'])
     return data
