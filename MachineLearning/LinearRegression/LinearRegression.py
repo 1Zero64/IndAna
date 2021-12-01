11b1241b2442ee92011ff017a3f24c245c8c52ff
1Zero64 <1Zero64@users.noreply.github.com>
2021-12-01 20:42:21 +0100

    Updated the heck out of LR. Cheers
    Signed-off-by: 1Zero64 <1Zero64@users.noreply.github.com>

--- MachineLearning/LinearRegression/LinearRegression.py
+++ MachineLearning/LinearRegression/LinearRegression.py
@@ -25,19 +25,17 @@ data_list = list(data_items)
 dataFrame = pd.DataFrame(data_list, columns=columns)
 dataFrame.date = pd.to_datetime(dataFrame.date)
 
-
-print(dataFrame)
-
-
-X = dataFrame.date.values.reshape(-1, 1)
+X = dataFrame["date"].values.reshape(-1, 1)
 Y = dataFrame["quantity"].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
 
 linear_regressor = LinearRegression().fit(X,Y)  # create object for the class
-linear_regressor.fit(X, Y)  # perform linear regression
-Y_predictions = ["2021-11-01", "2021-11-02"]
-Y_pred = linear_regressor.predict(dataFrame.date.values.astype(float).reshape(-1, 1))  # make predictions
+linear_regressor.fit(X, Y)
+Y_pred = linear_regressor.predict(dataFrame["date"].values.astype(float).reshape(-1, 1))
 
-plt.scatter(X, Y)
-plt.plot(X, Y_pred, color='red')
+plt.scatter(X, Y, s=10, label="Verkaufsmenge am Tag")
+plt.plot(X, Y_pred, color='red', label='Trend')
+plt.title("Verkaufsverlauf für das Produkt Apfel")
+plt.xlabel("Datum")
+plt.ylabel("Verkaufsmenge")
+plt.legend(loc="best")
 plt.show()
-
